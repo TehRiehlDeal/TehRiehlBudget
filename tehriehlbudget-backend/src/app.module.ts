f2cdb9f967e5d9a5603c1157275583699e23c1fd
Kevin Riehl <kevinriehl@gmail.com>
2026-04-12 17:02:17 -0700

    Scaffold NestJS backend with ValidationPipe and config

---
+++ tehriehlbudget-backend/src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { EncryptionModule } from './encryption/encryption.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { CategoriesModule } from './categories/categories.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { FilesModule } from './files/files.module';
+import { AggregationsModule } from './aggregations/aggregations.module';
+import { PlaidModule } from './plaid/plaid.module';
+import { AdvisorModule } from './advisor/advisor.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    EncryptionModule,
+    AccountsModule,
+    CategoriesModule,
+    TransactionsModule,
+    FilesModule,
+    AggregationsModule,
+    PlaidModule,
+    AdvisorModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
